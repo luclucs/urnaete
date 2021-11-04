ad1a12fa8debd0462777e358163b72c6f1e9aac2
luclucs <78450205+luclucs@users.noreply.github.com>
2021-11-04 00:29:45 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 console.log("só pra enviar no git")
+function deuClique() {
+    var popup = document.getElementById("meuPopup");
+    popup.classList.toggle("show");
+  }
+function refreshPagina(){
+  window.location.reload();
+}
