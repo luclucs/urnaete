cdf23f1fdd561c5d526e84bab3fd861825bd21ac
luclucs <78450205+luclucs@users.noreply.github.com>
2021-12-13 18:15:49 -0300

    diretório desnecessário

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-console.log("só pra enviar no git")
-function deuClique() {
-    var popup = document.getElementById("meuPopup");
-    popup.classList.toggle("show");
-  }
-function refreshPagina(){
-  window.location.reload();
-}
